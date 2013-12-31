ca9a24188198bd02adbc528f06601d796a7ecdc4
Justin Jiang <sleepboy@sleepboy.com>
2013-12-31 14:59:01 +0800

    调整use顺序，防止exception

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,9 +1,5 @@
 var settings = require('../../config/settings');
 
 module.exports = function (req, res, next) {
-    if (req.user && req.user.serialize) {
-        console.log(req.user.serialize());
-    }
-
-    res.render('home', { user: req.user ? req.user.serialize() : req.user });
+    res.render('home', { user: (req.user && req.user.serialize) ? req.user.serialize() : req.user });
 };
